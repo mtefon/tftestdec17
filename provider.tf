7977db259672d974ddcbcfa793d7da3de2e4763a
Mehmet Tefon <mtefon@gmail.com>
2022-12-23 20:54:27 -0500

    move backend block into the terraform block

--- provider.tf
+++ provider.tf
@@ -5,13 +5,13 @@ terraform {
       version = "3.36.0"
     }
   }
-}
 
-backend "remote" {
-  organization = "mtefon_inc"
-  workspaces {
-    name = "tftestdec17"
-  } 
+  backend "remote" {
+    organization = "mtefon_inc"
+    workspaces {
+      name = "tftestdec17"
+    } 
+  }  
 }
 
 provider "azurerm" {
